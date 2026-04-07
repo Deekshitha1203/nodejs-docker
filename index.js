6fb9b23be83110e5e05ab3c4fa57a3046a7bcb55
Deekshitha1203 <deekshithasuddamandu@gmail.com>
2026-04-07 15:30:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var http = require('http');
+require('@dynatrace/opentelemetry-azure-functions');
+
+const http = require('http')
 
 //create a server object:
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
   res.write('** Hey my app was deployed :) **'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+}).listen(3000); //the server object listens on port 80
